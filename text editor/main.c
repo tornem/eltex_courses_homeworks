e3868ea81f09759617a5a7f6347dd967d1b3299c
tornem <tornem@yandex.ru>
2018-07-01 20:30:51 +0700

    added help panel and working on the statusbar

--- text editor/main.c
+++ text editor/main.c
@@ -7,7 +7,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
-#include <form.h>
+#include <panel.h>
 
 void sig_winch(int signo)
 {
@@ -16,16 +16,21 @@ void sig_winch(int signo)
 	resizeterm(size.ws_row, size.ws_col);
 }
 
-void WriteInWin(char* read_buf, int max_y, int max_x)
-{
+void WriteInWin(WINDOW* info_win, char* read_buf, int max_y, int max_x)
+{	
+	char mode[] = "Edition mode ON";
 	int key;
 	int y1, x1;
+	
+	wprintw(info_win, "Edition mode ON!");
+	wrefresh(info_win);
+
 	while ((key = getch()) != 27){
 		switch (key){
 			case KEY_BACKSPACE:
 				getyx(stdscr, y1, x1);
 				if (x1 == 0){
-					mvaddch(y1, x1, EOF);
+					mvdelch(y1, x1);
 					read_buf[y1*max_y + x1] = ' ';
 					break;
 				} 
@@ -58,16 +63,54 @@ void SafeFile(int fd, char* read_buf)
 	
 }
 
+void InitHelp(WINDOW* w_help, PANEL* panel_help)
+{
+	w_help = newwin(10, 20, 2, 10);
+	wbkgd(w_help, COLOR_PAIR(2));
+	wclear(w_help);
+	wrefresh(w_help);
+
+	panel_help = new_panel(w_help);
+	update_panels();
+	doupdate();
+	hide_panel(panel_help);
+}
+
+void CallHelp(WINDOW* w_statusbar, PANEL* panel_help)
+{
+	int key;
+	//WINDOW* bind_win = panel_window(panel_help);
+	curs_set(FALSE);
+	show_panel(panel_help);
+	update_panels();
+	doupdate();
+	wclear(w_statusbar);
+	wprintw(w_statusbar, "Press h again for close help");
+	wrefresh(w_statusbar);
+	if ((key = getch()) == 'h'){
+		hide_panel(panel_help);
+		update_panels();
+		doupdate();
+		wclear(w_statusbar);
+		wprintw(w_statusbar, "Press 'h' for help");
+		wrefresh(w_statusbar);
+	}
+	curs_set(TRUE);
+}
+
+
 int main(int argc, char* argv[])
 {
-	FIELD *field;
-	int fd;
+	int fd; 
 	int y, x;
 	int max_y, max_x;
-	int key;
-	char read_buf[2400];
+	int key; 
+	char read_buf[2400]; 
 	char write_buf[2400];
-
+	WINDOW* w_statusbar;
+	WINDOW* w_help;
+	PANEL* panel_help;
+	
 	if (--argc == 0){
 		//вставить справку
 		printf("Please, enter filename what your need open/creat.\n");
@@ -90,7 +133,7 @@ int main(int argc, char* argv[])
 	start_color();
 	init_pair(1, COLOR_BLACK, COLOR_GREEN);
 	bkgd(COLOR_PAIR(1));
-	curs_set(1); 
+	curs_set(TRUE); 
 	printw("%s", read_buf);
 	noecho();
 	cbreak();
@@ -98,7 +141,25 @@ int main(int argc, char* argv[])
 	//raw();
 	getmaxyx(stdscr, max_y, max_x);
 
-	field = new_field(1, max_x, max_y, 0, 0, 0);
+	w_statusbar = subwin(stdscr, 1, max_x, max_y - 1, 0);
+	//w_help = subwin(stdscr, 10, 20, max_y/2 - 5, max_x/2 - 10);
+	init_pair(2, COLOR_BLACK, COLOR_BLUE);
+	wbkgd(w_statusbar, COLOR_PAIR(0));
+	wprintw(w_statusbar, "Press 'h' for help");
+
+	//Initialisation window for help-panel
+	// InitHelp(w_help, panel_help);
+	w_help = newwin(10, 20, 2, 10);
+	wbkgd(w_help, COLOR_PAIR(2));
+	wclear(w_help);
+	wrefresh(w_help);
+	mvwaddstr(w_help, 0, 0, "Use next keys for manage text editor:");
+
+
+	panel_help = new_panel(w_help);
+	update_panels();
+	doupdate();
+	hide_panel(panel_help);
 
 	while ((key = getch()) != 'q'){
 		getyx(stdscr, y, x);
@@ -116,11 +177,13 @@ int main(int argc, char* argv[])
 				move(y,--x);
 				break;
 			case 'i':
-				WriteInWin(read_buf, max_y, max_x);
+				WriteInWin(w_statusbar, read_buf, max_y, max_x);
 				break;
 			case 's':
 				SafeFile(fd, read_buf);
-
+			case 'h':
+				CallHelp(w_statusbar, panel_help);
+				break;
 		}
 	}
 	// FILE *fp;
