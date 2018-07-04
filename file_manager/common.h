1984d9688f7d6f324119899418014ea1797b0795
tornem <core.safecrlf>
2018-07-04 22:31:31 +0700

    rename

---
+++ file_manager/common.h
@@ -0,0 +1,20 @@
+#ifndef __MAIN__H__
+
+#define __MAIN__H__
+
+#include <curses.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <dirent.h>
+
+void Init_main_win(WINDOW** win_main_1, WINDOW** win_main_2, 
+									  int max_y, int max_x);
+
+void ListDir (char* dir_name, struct dirent*** name_list, int* cnt_dir);
+
+void RenderingListDir (WINDOW* win, struct dirent** name_list, 
+													int cnt_dir);
+
+#endif
